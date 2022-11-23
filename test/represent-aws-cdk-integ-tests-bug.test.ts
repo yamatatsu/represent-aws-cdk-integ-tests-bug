7806fa74b76bb3b5b686d527fe30bc57ce729b79
yamatatsu <the.kinnikuman@gmail.com>
2022-11-23 12:34:10 +0900

    Initial commit

---
+++ test/represent-aws-cdk-integ-tests-bug.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RepresentAwsCdkIntegTestsBug from '../lib/represent-aws-cdk-integ-tests-bug-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/represent-aws-cdk-integ-tests-bug-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RepresentAwsCdkIntegTestsBug.RepresentAwsCdkIntegTestsBugStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
